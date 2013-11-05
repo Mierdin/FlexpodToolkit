58b65bcd8f5a98576c7f704e057635bbe849ffe2
Matt Oswalt <matt@keepingitclassless.net>
2013-11-05 12:57:54 -0500

    Initial Commit
    Trying to get a general structure in place. Already moved functions to
    their own module, planning on doing this for nearly everything.

---
+++ main.ps1
@@ -0,0 +1,70 @@
+﻿<#
+
+ Name:         Flexpod Toolkit
+ Author:       Matthew Oswalt
+ Created:      6/10/2013
+ Description:  This script is the "main" script in a toolkit designed to automate Flexpod provisioning tasks.
+
+               Currently only tested and verified with the following platforms:
+                    *NetApp Release 8.2P2 Cluster-Mode
+                    *Cisco UCSM 2.1(3a)
+                    *Nexus 5596UP running 6.0(2)N2(2)
+
+
+               The modules are named according to use case. However, any function in any module can reach into any part of a flexpod for both configuring and retrieving information. 
+               For example, the function that creates boot LUNs works by first looking at the service profiles in UCS. 
+
+#>
+#####TODO#######
+#need to make this script create boot policies in UCS after looking at the Netapp target interfaces
+
+#Modularize:
+   #https://www.simple-talk.com/sysadmin/powershell/an-introduction-to-powershell-modules/
+   #http://social.technet.microsoft.com/Forums/windowsserver/en-US/941c1a0d-e359-4243-9fc5-82e95c2a4c9d/powershell-v3-and-importmodule-ps1
+   #http://msdn.microsoft.com/en-us/library/windows/desktop/dd878340(v=vs.85).aspx
+   # 
+
+
+Import-Module CiscoUcsPs
+Import-Module DataONTAP
+Import-Module .\modules\Storage.psm1
+
+#region VARs
+
+$NAipAddr = "10.102.0.50"
+$NAusername = "admin"
+$NApassword = "password"
+$NAportset = "fcoe_pset_1"
+$NAvserver = "DCA_FC_VS1"
+$NAbootVol = "/vol/FC_BootVol1/" #Needs to be of this format, including the forward slashes. LUN will be appended without any slashes
+
+$UCSipAddr = "10.102.1.5"
+$UCSusername = "admin"
+$UCSpassword = "password"
+$organization = "root"
+
+#endregion
+
+#region Establish Connections
+
+#Connect to Netapp, suppressing prompt
+$NASecPass = ConvertTo-SecureString $NApassword -AsPlainText -Force
+$NAcred = New-Object System.Management.Automation.PSCredential($NAusername, $NASecPass)
+Connect-NcController $NAipAddr -credential $NAcred
+
+#Connect to UCSM, suppressing prompt
+$UCSSecPass = ConvertTo-SecureString $UCSpassword -AsPlainText -Force
+$ucsmCreds = New-Object System.Management.Automation.PSCredential($UCSusername, $UCSSecPass)
+Disconnect-Ucs
+Connect-Ucs $UCSipAddr -Credential $ucsmCreds
+
+#endregion
+
+
+
+
+
+
+
+
+
---
+++ modules/Storage.psm1
@@ -0,0 +1,106 @@
+﻿
+#Create igroups and LUNs
+function createIGroupsAndLuns() {
+    
+#Reaches into a Cisco UCS Instance, and iterates through every service profile, creating Netapp initiator groups
+#for each one, and then putting each service profile's vHBA's WWPNs in the respective initiator groups.
+
+    #We only want Service Profile Instances, not Templates
+    $serviceProfiles = Get-UcsServiceProfile -Type instance -Org $organization
+
+    #Iterate through Service Profiles, creating boot luns and igroups, mapping as you go
+    foreach ($SP in $serviceProfiles) {
+
+        #create path to LUN 
+        $LUNPath = $NAbootVol + $SP.name + "_boot"
+
+        #check to see if lun by this path already exists
+        if (Get-NcLun -path  $LUNPath -Vserver $NAvserver) {
+            Write-Host "LUN already exists: " $SP.Name    
+        } Else {
+            #Need to finish flushing out this line
+            New-NcLun -path  $LUNPath -Size 10g -OsType vmware -Unreserved -vserver $NAvserver
+        }
+
+        #Populate array with existing vHBAs on this service profile
+        $vHBAs = $SP | Get-UcsVhba
+
+        #check to see if igroup by this name already exists
+        if (Get-NcIgroup -name $SP.Name) {
+            Write-Host "igroup already exists: " $SP.Name
+
+        } Else {
+            #Create the igroup first        
+            Write-Host "Creating igroup named " + $SP.Name
+            $newIgroup = New-NcIgroup -name $sp.Name -protocol fcp -portset $NAportset -type VMware -vserver $NAvserver
+
+            #Iterate through each vHBA, and add each WWPN to this igroup
+            foreach ($vHBA in $vHBAs) {
+                Write-Host "Adding WWPN " $vHBA.Addr
+                Add-NcIgroupInitiator -Initiator $vHBA.Addr -Name $SP.Name -vserver $NAvserver
+            }
+
+            #Adds the mapping at the tail end. Kept inside this if statement so it only ran on igroups that were created by this script, not existing ones
+            Add-NcLunMap -Path $LUNPath -InitiatorGroup $newIgroup.Name -vserver $NAvserver
+        }
+    }
+}
+
+
+#Generate FC switch config
+function generateFCSwitchConfig() {
+
+    $WWPNTableFabA = $null
+    $WWPNTableFabA = @{}
+    $WWPNTableFabB = $null
+    $WWPNTableFabB = @{}
+
+
+    #We only want Service Profile Instances, not Templates
+    $serviceProfiles = Get-UcsServiceProfile -Type instance -Org $organization
+
+    #Iterate through Service Profiles, creating boot luns and igroups, mapping as you go
+    foreach ($SP in $serviceProfiles) {
+
+        #Populate array with existing vHBAs on this service profile
+        $vHBAs = $SP | Get-UcsVhba
+
+        #Iterate through each vHBA, and add each WWPN to this igroup
+        foreach ($vHBA in $vHBAs) {
+            #Adding WWPN and name to hash table for later config generation
+            if($vHBA.SwitchId -eq "A") {
+                $WWPNTableFabA.Add($SP.Name + "_" + $vHBA.Name, $vHBA.Addr)
+            } else {
+                $WWPNTableFabB.Add($SP.Name + "_" + $vHBA.Name, $vHBA.Addr)
+            }
+
+        }
+    }
+
+    $ConfigFile = Read-Host "Enter path and file name for the config file"
+    New-Item -ItemType file $ConfigFile -Force
+
+    Add-Content $ConfigFile "!Fabric A Configuration"
+
+    Add-Content $ConfigFile "device-alias database"
+    $WWPNTableFabA.GetEnumerator() | % { 
+        #Write-Host "Current hashtable is: $($_.key)"
+        #Write-Host "Value of Entry 1 is: $($_.value)" 
+        $configLine = "device-alias name " + $($_.key) + " pwwn " + $($_.value)
+        Add-Content $ConfigFile $configLine
+    }
+    Add-Content $ConfigFile "device-alias commit"
+
+    Add-Content $ConfigFile "!"
+
+    Add-Content $ConfigFile "!Fabric A Configuration"
+
+    Add-Content $ConfigFile "device-alias database"
+    $WWPNTableFabB.GetEnumerator() | % { 
+        #Write-Host "Current hashtable is: $($_.key)"
+        #Write-Host "Value of Entry 1 is: $($_.value)" 
+        $configLine = "device-alias name " + $($_.key) + " pwwn " + $($_.value)
+        Add-Content $ConfigFile $configLine
+    }
+    Add-Content $ConfigFile "device-alias commit"
+}

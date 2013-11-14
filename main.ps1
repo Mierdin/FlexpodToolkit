f0ea6ce2d3da209f617e35b1f8e804d8e1c57da3
Matt Oswalt <matt@keepingitclassless.net>
2013-11-13 16:00:10 -0800

    made more file changes

--- main.ps1
+++ main.ps1
@@ -9,10 +9,12 @@
                     *NetApp Release 8.2P2 Cluster-Mode
                     *Cisco UCSM 2.1(3a)
                     *Nexus 5596UP running 6.0(2)N2(2)
+                    *Powershell 3.0 or above
 
 
                The modules are named according to use case. However, any function in any module can reach into any part of a flexpod for both configuring and retrieving information. 
                For example, the function that creates boot LUNs works by first looking at the service profiles in UCS. 
+               In summary, the module named CiscoUCS doesn't necessarily just reach into Cisco UCS.
 
 #>
 #####TODO#######
@@ -22,12 +24,26 @@
    #https://www.simple-talk.com/sysadmin/powershell/an-introduction-to-powershell-modules/
    #http://social.technet.microsoft.com/Forums/windowsserver/en-US/941c1a0d-e359-4243-9fc5-82e95c2a4c9d/powershell-v3-and-importmodule-ps1
    #http://msdn.microsoft.com/en-us/library/windows/desktop/dd878340(v=vs.85).aspx
-   # 
+   #http://msdn.microsoft.com/en-us/library/dd901839(v=vs.85).aspx
+   #http://msdn.microsoft.com/en-us/library/dd878284(v=vs.85).aspx
+   #http://msdn.microsoft.com/en-us/library/dd878340(v=vs.85).aspx
+   #http://stackoverflow.com/questions/10283622/load-multiple-modules-psm1-using-a-single-psd1-file
+   #http://technet.microsoft.com/library/hh849725.aspx
+   #http://stackoverflow.com/questions/14382579/relative-path-in-import-module
+   #
+   #
+   #
+   #
 
+
+#PowerShell v3 or higher
+#The -Force argument unloads the module first, which is good especially for dev. 
+Import-Module $PSScriptRoot\modules\Netapp.psm1 -Force
+Import-Module $PSScriptRoot\modules\VMware.psm1 -Force
+Import-Module $PSScriptRoot\modules\CiscoUCS.psm1 -Force
 
 Import-Module CiscoUcsPs
 Import-Module DataONTAP
-Import-Module .\modules\Storage.psm1
 
 #region VARs
 
@@ -41,7 +57,7 @@ $NAbootVol = "/vol/FC_BootVol1/" #Needs to be of this format, including the forw
 $UCSipAddr = "10.102.1.5"
 $UCSusername = "admin"
 $UCSpassword = "password"
-$organization = "root"
+$organization = "DI_DCA"
 
 #endregion
 
@@ -50,6 +66,7 @@ $organization = "root"
 #Connect to Netapp, suppressing prompt
 $NASecPass = ConvertTo-SecureString $NApassword -AsPlainText -Force
 $NAcred = New-Object System.Management.Automation.PSCredential($NAusername, $NASecPass)
+#Disconnect from Controller First
 Connect-NcController $NAipAddr -credential $NAcred
 
 #Connect to UCSM, suppressing prompt
@@ -60,11 +77,6 @@ Connect-Ucs $UCSipAddr -Credential $ucsmCreds
 
 #endregion
 
+#Create-IGroupsAndLuns
 
-
-
-
-
-
-
-
+Create-IGroupsAndLuns
